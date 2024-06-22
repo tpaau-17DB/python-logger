c87aa77de043103421a044fad65322a86bf4f1de
NicholasNDev <nicholas.neubauer.developer@gmail.com>
2024-06-22 19:45:57 +0200

    Ah, shit i should probably update the version

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 setup(
     name='logger',
-    version='1.0.0',
+    version='1.1.0',
+    author_email='nicholas.neubauer.developer@gmail.com',
+    description='Python logger package that can be used to debug or in the finished project.',
+    url='https://github.com/mikolajArchUser/python-logger',
     packages=['logger'],
     install_requires=[]
 )
