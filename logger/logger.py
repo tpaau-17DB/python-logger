3b6ba738ff4b1f94e75d372f445b64a083912349
NicholasNDev <nicholas.neubauer.developer@gmail.com>
2024-06-24 18:22:49 +0200

    Added a way to print the datetime automatically with every message.

--- logger/logger.py
+++ logger/logger.py
@@ -11,10 +11,25 @@ displayed and which should be discarded
 VERB=0 will display every log
 VERB=1 will display only warnings and errors
 VERB<2 will display only errors
+
+Logger will add date and time to all messages if PRINT_DATETIME is set to true.
 """
+
+from datetime import datetime as dt
 
 #Controlls which messages should be displayed
 VERBOSITY_LEVEL = 1
+
+#When set to true logger adds datetime to every message
+PRINT_DATETIME = True
+
+
+def set_print_datetime(switch):
+    """
+    Sets PRINT_DATETIME
+    """
+    global PRINT_DATETIME
+    PRINT_DATETIME = switch
 
 def set_verbosity(val):
     """
@@ -29,13 +44,21 @@ def get_verbosity():
     """
     return VERBOSITY_LEVEL
 
+def _get_datetime():
+    """
+    Returns datetime in printable format
+    """
+    if PRINT_DATETIME:
+        return f"[{dt.now().strftime('%H:%M:%S')}] "
+    return ""
+
 def log_mess(message, layer = 0):
     """
     prints messages in log format
     """
     if VERBOSITY_LEVEL > 0:
         return
-    print(" " * 2 * layer + f"\033[92m[LOG]\033[0m {message}")
+    print(" " * 2 * layer + f"\033[92m[LOG]\033[0m {_get_datetime()}{message}")
 
 
 def log_warn(message, layer = 0):
@@ -44,7 +67,7 @@ def log_warn(message, layer = 0):
     """
     if VERBOSITY_LEVEL > 1:
         return
-    print(" " * 2 * layer + f"\033[33m[WARN]\033[0m {message}")
+    print(" " * 2 * layer + f"\033[33m[WARN]\033[0m {_get_datetime()}{message}")
 
 
 def log_err(message, layer = 0):
@@ -53,4 +76,4 @@ def log_err(message, layer = 0):
     """
     if VERBOSITY_LEVEL > 2:
         return
-    print(" " * 2 * layer + f"\033[91m[ERR]\033[0m {message}")
+    print(" " * 2 * layer + f"\033[91m[ERR]\033[0m {_get_datetime()}{message}")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='logger',
-    version='1.1.0',
+    version='1.2.0',
     author_email='nicholas.neubauer.developer@gmail.com',
     description='Python logger package that can be used to debug or in the finished project.',
     url='https://github.com/mikolajArchUser/python-logger',
