10fcf047ec1d4269b001a5ad0e5efccb90c1bf65
NicholasNDev <nicholas.neubauer.developer@gmail.com>
2024-06-26 14:54:06 +0000

    Added a way to override default message priority

--- logger/logger.py
+++ logger/logger.py
@@ -5,14 +5,18 @@ log_mess prints message in '[LOG] <message>' format
 the same applies for log_warn and log_err, but [LOG]
 is replaced by [WARN] and [ERR] respectively
 
-VERBOSITY_LEVEL controlls which messages should be 
+VERBOSITY_LEVEL controlls which messages should be
 displayed and which should be discarded
 
 VERB=0 will display every log
 VERB=1 will display only warnings and errors
 VERB<2 will display only errors
 
-Logger will add date and time to all messages if PRINT_DATETIME is set to true.
+Passing override_prior=True to a function will print log
+no matter its priority
+
+Logger will add date and time to all messages if
+PRINT_DATETIME is set to true.
 """
 
 from datetime import datetime as dt
@@ -35,14 +39,16 @@ def set_verbosity(val):
     """
     sets the verbosity level
     """
-    global VERBOSITY_LEVEL;
+    global VERBOSITY_LEVEL
     VERBOSITY_LEVEL = val
+
 
 def get_verbosity():
     """
     returns verbosity value
     """
     return VERBOSITY_LEVEL
+
 
 def _get_datetime():
     """
@@ -52,28 +58,28 @@ def _get_datetime():
         return f"[{dt.now().strftime('%H:%M:%S')}] "
     return ""
 
-def log_mess(message, layer = 0):
+def log_mess(message, layer=0, override_prior=False):
     """
     prints messages in log format
     """
-    if VERBOSITY_LEVEL > 0:
+    if VERBOSITY_LEVEL > 0 and not override_prior:
         return
     print(" " * 2 * layer + f"\033[92m[LOG]\033[0m {_get_datetime()}{message}")
 
 
-def log_warn(message, layer = 0):
+def log_warn(message, layer=0, override_prior=False):
     """
     prints messages in warning format
     """
-    if VERBOSITY_LEVEL > 1:
+    if VERBOSITY_LEVEL > 1 and not override_prior:
         return
     print(" " * 2 * layer + f"\033[33m[WARN]\033[0m {_get_datetime()}{message}")
 
 
-def log_err(message, layer = 0):
+def log_err(message, layer=0, override_prior=False):
     """
     prints messages in error format
     """
-    if VERBOSITY_LEVEL > 2:
+    if VERBOSITY_LEVEL > 2 and not override_prior:
         return
     print(" " * 2 * layer + f"\033[91m[ERR]\033[0m {_get_datetime()}{message}")
